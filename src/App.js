7b0d8fe2e21ef05a83ea88691b65f9367f94a5fd
thiagovilarinholemes <lemes_vilarinho@yahoo.com.br>
2021-02-09 20:22:26 -0300

    Installed Material UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
 
+import theme from './theme';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
